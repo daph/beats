253e536b6eca86c15b74449cc6b5b8f4aaea6064
David Phillips <david@daviddouglasphillips.com>
2018-02-11 13:52:31 -0800

    Add some docs and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,45 @@
+//! # Beats 0.1.0
+//!
+//! Swatch internet time (.beats) crate for rust.
+//!
+//!
+
 extern crate chrono;
 
 use std::fmt;
 use chrono::prelude::*;
 
+/// Struct for representing a .beat
+#[derive(Debug, PartialEq)]
 pub struct Beat {
     trunc: f64,
     fract: f64
 }
 
 impl Beat {
+    /// Create a Beat for the current time
+    ///
+    /// # Example
+    /// ~~~
+    /// use beats::Beat;
+    ///
+    /// let beat_now = Beat::now();
+    /// println!("It is currently: {}", beat_now)
+    /// ~~~
     pub fn now() -> Beat {
         let now: DateTime<Utc> = Utc::now();
         calculate_beats(now)
     }
 
-    pub fn from<T: TimeZone>(time: DateTime<T>) -> Beat {
+    /// Create a Beat from a [Chrono](https://docs.rs/chrono) DateTime.
+    pub fn from_dt<Tz: TimeZone>(time: DateTime<Tz>) -> Beat {
         calculate_beats(time)
     }
 }
 
 impl fmt::Display for Beat {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:.3}", self.trunc + self.fract)
+        write!(f, "@{:.3}", self.trunc + self.fract)
     }
 }
 
@@ -42,4 +60,19 @@ fn wrap(beat: Beat) -> Beat {
     };
 
     Beat { trunc, fract: beat.fract }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn check_wrap() {
+        let beat = Beat {
+            trunc: 1000.0,
+            fract: 0.001
+        };
+
+        assert_eq!(wrap(beat), Beat {trunc: 0.0, fract: 0.001});
+    }
 }
